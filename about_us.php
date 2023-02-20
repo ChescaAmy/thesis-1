68d75127dad2da9b80db946226a9a28c2ca48be4
Jem Angeles <105427704+febibeans@users.noreply.github.com>
2023-02-20 20:25:44 +0800

    Add files via upload

---
+++ about_us.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="UTF-8"> 
+        <title>About Us</title>
+        <link rel="stylesheet" href="stylesheet.css">
+    </head>
+    <body>
+        <div class="one">
+            <a href="home.php" style="font-size:20px; font-weight:70px; text-decoration:none; color:black;">Estillore Medical Clinic</a>
+        </div>
+        <div class="two">
+            <div class="redirects">
+                <a href="about_us.php" style="font-size:20px; font-weight:50px; text-decoration:none;"><h2>About Us</h2></a>
+                <a href="login.php" style="font-size:20px; font-weight:50px; text-decoration:none;"><h2>Log-In</h2></a>
+            </div>
+        </div>    
+        <div class="missionvision">
+            <div class="mission" style="font-size:30px; font-weight:150px; text-align:center; padding: 20px;">
+                <p>Our mission is to improve the health and well-being of the people we serve by ensuring highly effective, community-based support and care.</p>
+            </div>
+            <div class="vision" style="font-size:30px; font-weight:150px; text-align:center; padding: 20px;">
+                <p>To inspire hope and contribute to health and well-being by providing the best care to every patient through integrated clinical practice, education and research.</p>
+            </div>
+        </div>
+    </body>
+</html>
